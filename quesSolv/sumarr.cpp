a368385fd2d18e877f90dc5f7a472bccc4e3e86f
Antarip Chatterjee <chatterjeeantarip1234@gmail.com>
2022-08-04 10:58:40 +0530

    commit

---
+++ quesSolv/sumarr.cpp
@@ -0,0 +1,18 @@
+//Sum of an array
+//input [1,2,3,4] output 10
+
+#include<iostream>
+using namespace std;
+int sumarr(int arr[],int n){
+    int sum = 0;
+    for(int i = 0;i < n;i++){
+        sum += arr[i];
+    }
+    return sum;
+}
+int main(){
+    int arr[] = {1,2,3,4};
+    int n = sizeof(arr)/sizeof(arr[0]);
+    cout<<sumarr(arr,n);
+    return 0;
+}
