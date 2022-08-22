30b2a9b96f4470e936e4e1b44ac8a93eaa38eddb
Antarip chatterjee <85601268+AntaripChat@users.noreply.github.com>
2022-08-22 10:45:33 +0530

    Update fibo.js

--- quesSolv/fibo.js
+++ quesSolv/fibo.js
@@ -7,3 +7,22 @@ function fibo(n){
 };
 
 console.log(fibo(4));
+
+
+//print fibonacci number
+
+function fibo(n,arr){
+    if(n ==0 || n == 1){
+        arr[n] = n;
+        return n;
+    }
+    let value = fibo(n - 1,arr) + fibo(n - 2,arr);
+    arr[n] = value;
+    return value;
+};
+
+let arr = [];
+
+fibo(4,arr);
+
+console.log(arr);
